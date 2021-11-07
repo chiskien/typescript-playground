2c8a9fb403661f979288e9e2abe2acbc61c6b2f0
ChisKien <chiskien03214@gmail.com>
2021-11-07 08:47:17 +0700

    Create Enum

---
+++ dist/class-management/model/class.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Classroom = void 0;
+var Classroom = /** @class */ (function () {
+    function Classroom() {
+    }
+    return Classroom;
+}());
+exports.Classroom = Classroom;
+//# sourceMappingURL=class.js.map
--- src/typescript_basic/index.ts
+++ src/typescript_basic/index.ts
@@ -4,3 +4,15 @@ import { filter } from "./polymorphism";
 let array = [2, 4, 15, 61, 78, 745];
 let subArray = filter(array, (index) => index < 3);
 console.table(subArray);
+
+enum Employee {
+  ProjectManager,
+  FrontendDeveloper,
+  BackendDeveloper,
+  Tester,
+  ScrumMaster,
+  ProductOwner,
+  BusinessAnalyst,
+  UXUIDesigner,
+}
+console.log(Employee[2]);
