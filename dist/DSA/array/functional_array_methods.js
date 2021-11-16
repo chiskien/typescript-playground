194f91c9a6e3aa994b14326a86b3569157335494
ChisKien <chiskien03214@gmail.com>
2021-11-16 09:37:41 +0700

    function array

---
+++ dist/DSA/array/functional_array_methods.js
@@ -0,0 +1,12 @@
+//map function applied passed function transformation to every element in the array
+// and return new array with those transformation applied
+console.log([1, 2, 4, 1, 51, 5].map(function (value) {
+    return value * 10;
+}));
+//filter
+//filter returns only those elemetns of an array that meet a passed condition parameter
+console.log([12312, 112, 45, 15, 1, 51].filter(function (value) {
+    return value > 1000;
+}));
+//reduce
+//# sourceMappingURL=functional_array_methods.js.map
---
+++ dist/DSA/array/map.js
@@ -0,0 +1,6 @@
+//map function applied passed function transformation to every element in the array
+// and return new array with those transformation applied
+console.log([1, 2, 4, 1, 51, 5].map(function (value) {
+    return value * 10;
+}));
+//# sourceMappingURL=map.js.map
