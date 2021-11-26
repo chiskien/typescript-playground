f15d3eb3eef07801cfb20d7e25a73103c2220de5
ChisKien <chiskien03214@gmail.com>
2021-11-26 10:06:31 +0700

    Hello Worlds

--- dist/DSA/main.js
+++ dist/DSA/main.js
@@ -1,8 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var search_1 = require("./algorithms/search");
-// let string: string[] = ["a", "lazy", "fox", "jumps"];
-// console.log(joinString(string));
-var arrayOfNumbers = [1, 214, 451, 51, 421, 4, 214, 15];
-console.log((0, search_1.linearSearch)(arrayOfNumbers, 214));
+console.log("Hello Worlds");
 //# sourceMappingURL=main.js.map
--- src/DSA/main.ts
+++ src/DSA/main.ts
@@ -1,8 +1,1 @@
-import { joinString } from "./string/string";
-import { linearSearch } from "./algorithms/search";
-
-// let string: string[] = ["a", "lazy", "fox", "jumps"];
-// console.log(joinString(string));
-
-let arrayOfNumbers = [1, 214, 451, 51, 421, 4, 214, 15];
-console.log(linearSearch(arrayOfNumbers, 214));
+console.log("Hello Worlds");
